a1db856afb3addcb783882c8e1572b50202e6986
FredaFei <964959033@qq.com>
2019-03-20 16:34:56 +0800

    更新测试用例，测试环境测试

--- packages/__tests__/index.unit.tsx
+++ packages/__tests__/index.unit.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import Button from '../button'
+import Button from '../button/button'
 
 describe('Button', () => {
   it('first test', () => {
