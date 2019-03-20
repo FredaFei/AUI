c72d423524aca590fba134badeac75abd48c387d
FredaFei <964959033@qq.com>
2019-03-20 16:29:40 +0800

    添加 icon 组件

---
+++ packages/icon/icon.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import * as PropTypes from 'prop-types'
+import { classNames } from '../utils/'
+import './svg.js'
+import './style'
+
+interface IIconProps {
+  name: string
+  style?: React.CSSProperties
+  rotate?: number
+  spin?: boolean
+  className?: string
+  onClick?: React.MouseEventHandler
+}
+const componentName = 'Icon'
+
+const Icon: React.SFC<IIconProps> = (props: IIconProps) => {
+  const { name, style, rotate, spin, className, onClick } = props
+  const classes = classNames(componentName, className, {
+    'icon-spin': spin || name === 'loading'
+  })
+  const styles = Object.assign(
+    {},
+    style,
+    rotate && {
+      transform: `rotate(${rotate}deg)`
+    }
+  )
+  return (
+    <svg className={classes} style={styles}>
+      <use xlinkHref={`#i-${name}`} onClick={onClick} />
+    </svg>
+  )
+}
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  className: PropTypes.string
+}
+export default Icon
