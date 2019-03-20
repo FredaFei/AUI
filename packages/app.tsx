14750171f53bccefeee1d20027b431dd0e2eacb8
FredaFei <964959033@qq.com>
2019-03-20 16:32:47 +0800

    组件应用实例

---
+++ packages/app.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+// import Button from './button/button'
+import Icon from './icon/icon'
+
+class App extends Component {
+  onClick = ()=>{
+    console.log(1)
+  }
+  render() {
+    return (
+      <div className="App">
+        <Icon name="settings" onClick={this.onClick} />
+        <Icon name="loading" onClick={this.onClick} />
+        <Icon name="left" rotate={90} style={{ fill: 'red'}} onClick={this.onClick} />
+        <Icon name="right" className="haha" onClick={this.onClick} />
+        {/* <Button /> */}
+      </div>
+    )
+  }
+}
+
+export default App
--- packages/index.tsx
+++ packages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import Button from './button'
-ReactDOM.render(<Button />, document.getElementById('root'))
+import App from './app'
+
+ReactDOM.render(<App />, document.getElementById('root'))
