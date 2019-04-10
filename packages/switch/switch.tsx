3b50671b26872bb2783be00f5f79d4f35aabcd2d
FredaFei <964959033@qq.com>
2019-04-10 15:03:34 +0800

    switch 组件添加开启时动画

--- packages/switch/switch.tsx
+++ packages/switch/switch.tsx
@@ -13,55 +13,88 @@ interface IProps {
 }
 interface IState {
   checked: boolean
+  position: object
 }
 const componentName = 'Switch'
 
 class Switch extends React.Component<IProps, IState> {
+  public static defaultProps = {
+    disabled: false,
+    defaultChecked: false
+  }
+  public static propTypes = {
+    disabled: PropTypes.bool,
+    checked: PropTypes.bool,
+    onChange: PropTypes.func,
+    className: PropTypes.string,
+    style: PropTypes.object
+  }
+  private rippleElement: React.RefObject<HTMLSpanElement>
+  private rippleParentElement: React.RefObject<HTMLLabelElement>
   constructor(props: IProps) {
     super(props)
+    this.rippleElement = React.createRef()
+    this.rippleParentElement = React.createRef()
     this.state = {
-      checked: props.defaultChecked || props.checked || false
+      checked: props.defaultChecked || false,
+      position: {}
     }
   }
-  onClick = e => {
-    console.log(e)
+  public static getDerivedStateFromProps(nextProps: IProps, prevState: IState) {
+    if ('checked' in nextProps && nextProps.checked !== prevState.checked) {
+      return { checked: nextProps.checked }
+    }
+    return null
+  }
+  public onRippleEffect = (): any => {
+    const targetEl = this.rippleParentElement.current
+    const rippleEl = this.rippleElement.current
+    if (!targetEl || !rippleEl) {
+      return false
+    }
+    rippleEl.classList.remove('active')
+    const { width, height } = targetEl.getBoundingClientRect()
+    const R = width < height ? height : width
+    this.setState({
+      position: {
+        width: `${R * 2}px`,
+        height: `${R * 2}px`,
+        marginLeft: `-${R}px`,
+        marginTop: `-${R}px`
+      }
+    })
+    rippleEl.classList.add('active')
+  }
+  public onClick = (event: React.MouseEvent<HTMLElement>): any => {
     const { disabled, onChange } = this.props
     if (disabled) {
       return false
     }
+    this.onRippleEffect()
     this.setState(state => ({ checked: !state.checked }))
-    onChange && onChange(this.state.checked, e)
+    onChange && onChange(this.state.checked, event)
   }
   render() {
-    const {
-      defaultChecked,
-      checked,
-      disabled,
-      style,
-      className,
-      onChange
-    } = this.props
-    const active = this.state.checked && 'active'
-    const disabled = disabled && 'disabled'
-    const classes = classNames(componentName, [className], { active, disabled })
+    const { position, checked } = this.state
+    const { disabled, style, className } = this.props
+    const isActive = checked && 'active'
+    const isDisabled = disabled && 'disabled'
+    const classes = classNames(componentName, [className, isActive, isDisabled])
     const styles = Object.assign({}, style)
     return (
       <label className={classes} style={styles} onClick={this.onClick}>
-        <span className={classNames('switch-core', { active, disabled })} />
-        {/* <span className="ripple" /> */}
+        <span
+          className={classNames('switch-core', [isActive])}
+          ref={this.rippleParentElement}
+        >
+          <span
+            className={classNames('switch-ripple', [isActive])}
+            style={position}
+            ref={this.rippleElement}
+          />
+        </span>
       </label>
     )
   }
-}
-Switch.defaultProps = {
-  disabled: false,
-  checked: false
-}
-Switch.propTypes = {
-  disabled: PropTypes.bool,
-  checked: PropTypes.bool,
-  onChange: PropTypes.func,
-  className: PropTypes.string,
-  style: PropTypes.object
 }
 export default Switch
