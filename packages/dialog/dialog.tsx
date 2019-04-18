4165bc58ce2c5cb3055605920eb8c68aece9cfd8
FredaFei <964959033@qq.com>
2019-04-18 18:10:12 +0800

    更新 dialog 组件功能

--- packages/dialog/dialog.tsx
+++ packages/dialog/dialog.tsx
@@ -1,22 +1,33 @@
 import * as React from 'react'
+import ReactDOM from 'react-dom'
 import { ReactElement, ReactFragment } from 'react'
 import * as PropTypes from 'prop-types'
 import classes, { createScopedClasses } from '../utils/classnames'
 import Icon from '../icon/icon'
+import Button from '../button/button'
 import './style'
 
 const componentName = 'dialog'
 const sc = createScopedClasses(componentName)
 interface IProps extends IStyledProps {
   visible: boolean
+  mask?: {
+    visible?: boolean
+    closable?: boolean
+  }
   title?: string
   footer?: ReactFragment | ReactElement
   onClose?: React.MouseEventHandler
+  onConfirm?: React.MouseEventHandler
 }
 
 class Dialog extends React.Component<IProps> {
   static defaultProps = {
-    visible: false
+    visible: false,
+    mask: {
+      visible: true,
+      closable: true
+    }
   }
   static propTypes = {
     onClose: PropTypes.func
@@ -24,26 +35,146 @@ class Dialog extends React.Component<IProps> {
   constructor(props: IProps) {
     super(props)
   }
+  onClose: React.MouseEventHandler = e => {
+    const { onClose } = this.props
+    onClose && onClose(e)
+  }
+  onOk: React.MouseEventHandler = e => {
+    const { onClose, onConfirm } = this.props
+    onClose && onClose(e)
+    onConfirm && onConfirm(e)
+  }
   render() {
-    const { visible, title, style, footer, children } = this.props
+    const {
+      visible,
+      mask,
+      title,
+      style,
+      className,
+      footer,
+      children
+    } = this.props
     return (
-      visible && (
-        <React.Fragment>
-          <div className={sc('wrapper')} style={style}>
-            <div className={sc('mask')} />
-            <div className={sc('')}>
-              <div className={sc('close')}>
-                <Icon name="close"  />
-              </div>
-              <div className={sc('header')}>{title}</div>
-              <div className={sc('body')}>{children}</div>
-              <div className={sc('footer')}>{footer}</div>
+      visible &&
+      ReactDOM.createPortal(
+        <div className={classes(sc('wrapper'), className)} style={style}>
+          {mask && mask.visible && (
+            <div
+              className={sc('mask')}
+              onClick={mask.closable ? this.onClose : undefined}
+            />
+          )}
+          <div className={sc('')}>
+            <div className={sc('close')} onClick={this.onClose}>
+              <Icon name="close" />
             </div>
+            {title && <div className={sc('header')}>{title}</div>}
+            <div className={sc('body')}>{children}</div>
+            {footer && <div className={sc('footer')}>{footer}</div>}
           </div>
-        </React.Fragment>
+        </div>,
+        document.body
       )
     )
   }
+}
+
+export const alert = (content: string, onNo) => {
+  const onClose = () => {
+    React.cloneElement(component, { visible: false })
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+  }
+  const component = (
+    <Dialog
+      visible={true}
+      onClose={onClose}
+      footer={
+        <Button
+          onClick={() => {
+            onClose()
+            onNo()
+          }}
+        >
+          确定
+        </Button>
+      }
+    >
+      {content}
+    </Dialog>
+  )
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component, div)
+}
+export const confirm = (content, onYes, onNo) => {
+  const onClose = () => {
+    React.cloneElement(component, { visible: false })
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+  }
+  const onCancel = () => {
+    onClose()
+    onNo()
+  }
+  const onConfirm = () => {
+    onClose()
+    onYes()
+  }
+  const component = (
+    <Dialog
+      visible={true}
+      onClose={onClose}
+      footer={
+        <React.Fragment>
+          <Button onClick={onCancel}>取消</Button>
+          <Button onClick={onConfirm} className={sc('btn-confirm')}>
+            确定
+          </Button>
+        </React.Fragment>
+      }
+    >
+      {content}
+    </Dialog>
+  )
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component, div)
+}
+export const modal = (content, onYes, onNo) => {
+  const onClose = () => {
+    React.cloneElement(component, { visible: false })
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+  }
+  const onCancel = () => {
+    onClose()
+    onNo()
+  }
+  const onConfirm = () => {
+    onClose()
+    onYes()
+  }
+  const component = (
+    <Dialog
+      visible={true}
+      onClose={onClose}
+      footer={
+        <React.Fragment>
+          <Button onClick={onCancel}>取消</Button>
+          <Button onClick={onConfirm} className={sc('btn-confirm')}>
+            确定
+          </Button>
+        </React.Fragment>
+      }
+    >
+      {content}
+    </Dialog>
+  )
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component, div)
+  return onClose
 }
 
 export default Dialog
