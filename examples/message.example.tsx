2c1b386963b14e0ea1db7331a6fdf3a2494e44b1
FredaFei <964959033@qq.com>
2019-04-24 15:39:50 +0800

    更新message 组件示例

--- examples/message.example.tsx
+++ examples/message.example.tsx
@@ -5,7 +5,7 @@ import message from '../packages/message/message'
 
 export default function(props: any) {
   const [text, setText] = useState('default')
-  const i = 3
+  const i = 5
   const onCloseMessage = key => {
     return () => setText(key)
   }
@@ -17,29 +17,28 @@ export default function(props: any) {
     message[key](`This is a ${key} message`, i - 1, onCloseMessage(key))
   }
   const onManualClose = () => {
-    // message.open({
-    //   content: 'This is a normal message',
-    //   duration:0,
-    //   onClose: () => {
-    //     console.log('muls')
-    //   }
-    // })
     message.info('This is message', 0, () => {
-      console.log('muls')
+      message.success('我已经关闭了！')
     })
   }
   return (
     <div className="exp-box">
-      <Button onClick={() => onMessage('open')}>message open</Button>
-      <Button onClick={() => onMessage('success')}>success</Button>
-      <Button onClick={() => onMessage('info')}>info</Button>
-      <Button onClick={() => onMessage('warning')}>warning</Button>
-      <Button onClick={() => onMessage('error')}>error</Button>
-      <Button onClick={() => onMessage('loading')}>loading</Button>
-      <h4 style={{ marginTop: '20px' }}>
-        current text: <span style={{ color: '#1890ff' }}>{text}</span>
-      </h4>
-      <Button onClick={onManualClose}>手动关闭</Button>
+      <div className="exp-section">
+        <h3>基础调用</h3>
+        <Button onClick={() => onMessage('open')}>message open</Button>
+        <Button onClick={() => onMessage('success')}>success</Button>
+        <Button onClick={() => onMessage('info')}>info</Button>
+        <Button onClick={() => onMessage('warning')}>warning</Button>
+        <Button onClick={() => onMessage('error')}>error</Button>
+        <Button onClick={() => onMessage('loading')}>loading</Button>
+        <h4 style={{ marginTop: '20px' }}>
+          current text: <span style={{ color: '#1890ff' }}>{text}</span>
+        </h4>
+      </div>
+      <div className="exp-section">
+        <h3>基础调用</h3>
+        <Button onClick={onManualClose}>手动关闭</Button>
+      </div>
     </div>
   )
 }
