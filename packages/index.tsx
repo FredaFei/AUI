736a37d5898da56e882a8be3da9fc28a45fc4090
FredaFei <964959033@qq.com>
2019-04-28 11:38:03 +0800

    导入/出 grid 组件

--- packages/index.tsx
+++ packages/index.tsx
@@ -20,6 +20,8 @@ declare global {
 export { default as Button } from './button/button'
 export { default as ButtonGroup } from './button/buttonGroup'
 export { default as Icon } from './icon/icon'
+export { default as Row } from './grid/row'
+export { default as Col } from './grid/col'
 export { default as Dialog } from './dialog/dialog'
 export { default as Message } from './message/message'
 export { default as Switch } from './switch/switch'
