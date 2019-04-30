a567a1147e46ecfd0b1365c9b891eea3130935aa
FredaFei <964959033@qq.com>
2019-04-30 10:42:39 +0800

    更新 表单验证器

--- packages/form/validator.tsx
+++ packages/form/validator.tsx
@@ -1,35 +1,42 @@
 import { FormValue } from './form'
 
+function isEmpty(value: any) {
+  return value === '' || value === null || value === undefined
+}
+
 type rules = Array<FormRules>
 interface FormRules {
   key: string
+  label: string
   required?: boolean
   minLength?: number
   maxLength?: number
   pattern?: RegExp
 }
-
+export const noErrors = (value: any) => {
+  return Object.values(value).length === 0
+}
 const validator = (formValue: FormValue, rules: rules) => {
-  const result:string = []
-  rules.map(r => {
-    if (!result[r.key]) { result[r.key] = {}}
-    if (
-      r.required &&
-      (formValue[r.key] === '' ||
-        formValue[r.key] === null ||
-        formValue[r.key] === undefined)
-    ) {
-      // result.push({ r.key: `${r.key}的值不能为空`})
+  const result: any = {}
+  const addErrors = (key: string, message: string) => {
+    if (!result[key]) {
+      result[key] = []
     }
-    if (
-      !(
-        formValue[r.key] === '' ||
-        formValue[r.key] === null ||
-        formValue[r.key] === undefined
-      ) &&
-      formValue[r.key].length < r.minLength!
-    ) {
-      // result[r.key] = `${r.key}的最小长度为${r.minLength!}`
+    result[key].push(message)
+  }
+  rules.map(r => {
+    let value = formValue[r.key]
+    if (isEmpty(value) && r.required) {
+      addErrors(r.key, `${r.label}的值不能为空`)
+    }
+    if (!isEmpty(value) && value.length < r.minLength!) {
+      addErrors(r.key, `${r.label}的最小长度为${r.minLength!}`)
+    }
+    if (!isEmpty(value) && value.length > r.maxLength!) {
+      addErrors(r.key, `${r.label}的最大长度为${r.maxLength!}`)
+    }
+    if (!isEmpty(value) && r.pattern && r.pattern.test(value)) {
+      addErrors(r.key, `${r.label}的格式不正确`)
     }
   })
   return result
