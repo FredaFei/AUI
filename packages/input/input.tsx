73bed73aeb6b28bbe6709c001289055445ddfb6e
FredaFei <964959033@qq.com>
2019-05-05 16:00:41 +0800

    更新 input 组件

--- packages/input/input.tsx
+++ packages/input/input.tsx
@@ -9,6 +9,8 @@ const sc = createScopedClasses(componentName)
 
 export interface IProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  before?: string | React.ReactNode
+  after?: string | React.ReactNode
   label?: string
   labelPosition?: 'left' | 'top'
   error?: string
@@ -21,12 +23,11 @@ export interface IProps
     | 'password'
     | 'email'
     | 'date'
-    | 'datetime'
-    | 'datetime-local'
+    | 'time'
     | 'search'
     | 'tel'
-    | 'time'
     | 'url'
+  onEnter?: React.KeyboardEventHandler<HTMLInputElement>
 }
 
 const Input: React.FunctionComponent<IProps> = props => {
@@ -39,6 +40,9 @@ const Input: React.FunctionComponent<IProps> = props => {
     type,
     style,
     className,
+    before,
+    after,
+    onEnter,
     ...rest
   } = props
   const class1 = {
@@ -47,14 +51,33 @@ const Input: React.FunctionComponent<IProps> = props => {
   }
   const class2 = {
     'error-bottom': errorPosition === 'bottom',
-    'label-right': errorPosition === 'right'
+    'error-right': errorPosition === 'right'
+  }
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode === 13) {
+      onEnter && onEnter.call(null, e)
+    }
+    props.onKeyDown && props.onKeyDown.call(null, e)
   }
   return (
-    <div className={classes(sc('wrapper', class1, size), className)}>
+    <div
+      className={sc('wrapper', class1, {
+        big: size === 'big',
+        small: size === 'small'
+      })}
+    >
       {label && <div className={sc('label')}>{label}</div>}
       <div className={sc('inputAndError')}>
         <div className={sc('content')}>
-          <input className={sc('')} type={type} style={style} {...rest} />
+          {before && <span className={sc('before')}>{before}</span>}
+          <input
+            className={classes(sc(''), className)}
+            type={type}
+            style={style}
+            {...rest}
+            onKeyDown={onKeyDown}
+          />
+          {after && <span className={sc('after')}>{after}</span>}
         </div>
         {error && <span className={sc('error', class2)}>{error}</span>}
       </div>
@@ -63,6 +86,11 @@ const Input: React.FunctionComponent<IProps> = props => {
 }
 
 Input.displayName = componentName
+Input.defaultProps = {
+  errorPosition: 'right',
+  type: 'text',
+  labelPosition: 'top'
+}
 Input.propTypes = {}
 
 export default Input
