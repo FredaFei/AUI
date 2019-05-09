026f8e1da3f3097e9cc6cee8879b582126c3d031
FredaFei <964959033@qq.com>
2019-05-09 16:16:44 +0800

    更新 pager 组件示例

--- examples/pager.example.tsx
+++ examples/pager.example.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react'
+import { useState } from 'react'
 import Pager from '../packages/pager/pager'
 
-
-export default function (props:any) {
+export default function(props: any) {
+  const [page1, setPage1] = useState(2)
+  const [page2, setPage2] = useState(3)
   return (
     <div className="exp-box">
       <div className="exp-section">
         <h3>基础应用</h3>
-        <Pager></Pager>
+        <Pager
+          defaultCurrent={4}
+          total={5}
+          onChange={page => {
+            console.log(page)
+          }}
+        />
+        <Pager current={page1} total={11} onChange={page => setPage1(page)} />
       </div>
       <div className="exp-section">
-        <h3>按钮组合</h3>
-      </div>
-      <div className="exp-section">
-        <h3>不可用状态</h3>
+        <h3>可跳转</h3>
+        <Pager
+          visibleQuickJumper
+          current={page2}
+          total={50}
+          onChange={page => setPage2(page)}
+        />
       </div>
     </div>
   )
