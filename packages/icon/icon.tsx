0e2f9e397c6c3d7772b249e5110386b7e381df9a
FredaFei <964959033@qq.com>
2019-06-27 17:30:03 +0800

    更新 Icon 组件

--- packages/icon/icon.tsx
+++ packages/icon/icon.tsx
@@ -1,42 +1,40 @@
 import * as React from 'react'
 import * as PropTypes from 'prop-types'
-import classes, { createScopedClasses } from '../utils/classnames'
+import classes, {createScopedClasses} from '../utils/classnames'
 import './svg.js'
 import './style'
 
-interface IProps {
-  name: string
-  style?: React.CSSProperties
-  rotate?: number
-  spin?: boolean
-  className?: string
-  onClick?: React.MouseEventHandler
+interface IProps extends IStyledProps {
+    name: string
+    rotate?: number
+    spin?: boolean
+    onClick?: () => void
 }
+
 const componentName = 'Icon'
 const sc = createScopedClasses(componentName)
 
-const Icon: React.SFC<IProps> = (props: IProps) => {
-  const { name, style, rotate, spin, className, onClick } = props
-  const wrapClasses = classes(sc(), [spin || (name === 'loading' && 'icon-spin')], className)
-  const styles = Object.assign(
-    {},
-    style,
-    rotate && {
-      transform: `rotate(${rotate}deg)`,
-      transition: `transform .2s ease`
-    }
-  )
-  return (
-    <svg className={wrapClasses} style={styles}>
-      <use xlinkHref={`#i-${name}`} onClick={onClick} />
-    </svg>
-  )
+const Icon: React.FunctionComponent<IProps> = (props: IProps) => {
+    const {name, style, rotate, spin, className, onClick, ...rest} = props
+    const wrapClasses = classes(sc(), [spin || (name === 'loading' && 'icon-spin')], className)
+    const styles = Object.assign(
+        {},
+        style,
+        rotate && {
+            transform: `rotate(${rotate}deg)`,
+            transition: `transform .2s ease`
+        }
+    )
+    return (
+        <svg className={wrapClasses} style={styles} onClick={onClick} {...rest}>
+            <use xlinkHref={`#i-${name}`}/>
+        </svg>
+    )
 }
 Icon.propTypes = {
-  name: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-  className: PropTypes.string,
-  style: PropTypes.object
+    name: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    style: PropTypes.object
 }
 Icon.displayName = componentName
 export default Icon
