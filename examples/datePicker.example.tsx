097e74c47df6d029e694fba997878731f8f13a3a
FredaFei <964959033@qq.com>
2019-06-28 16:31:28 +0800

    更新 datePicker 组件示例

--- examples/datePicker.example.tsx
+++ examples/datePicker.example.tsx
@@ -6,14 +6,18 @@ import {useState} from "react";
 export default function (props: any) {
 
     const [d, setD] = useState(new Date());
+    const onChange = (date: Date) => {
+        console.log(date)
+    }
     return (
         <div className="exp-box">
             <div className="exp-section">
                 <h3>基础应用</h3>
-                <DatePicker value={d} onChange={date => setD(date)}>hahah</DatePicker>
+                <DatePicker value={d} onChange={day => setD(day)}/>
             </div>
             <div className="exp-section">
                 <h3>不可用状态</h3>
+                {/*<DatePicker onChange={onChange}/>*/}
             </div>
         </div>
     )
