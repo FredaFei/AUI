b2eeefa67e3e560142f8e544bfbbba705af44e46
FredaFei <964959033@qq.com>
2019-07-02 09:40:58 +0800

    更新 datePicker 组件示例

--- examples/datePicker.example.tsx
+++ examples/datePicker.example.tsx
@@ -16,7 +16,7 @@ export default function (props: any) {
                 <DatePicker value={d} onChange={(day:Date|string) => setD(day)}/>
             </div>
             <div className="exp-section">
-                <h3>不可用状态</h3>
+                <h3>日期默认为空</h3>
                 <DatePicker onChange={onChange}/>
             </div>
         </div>
