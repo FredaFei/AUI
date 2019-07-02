ddf7c133748352113e913c317e18cbc3075d9eb4
FredaFei <964959033@qq.com>
2019-07-02 17:38:42 +0800

    更新 clickOutside 组件

--- packages/clickOutside/clickOutside.tsx
+++ packages/clickOutside/clickOutside.tsx
@@ -1,24 +1,63 @@
 import * as React from 'react'
-// import * as PropTypes from 'prop-types'
-import classes, { createScopedClasses } from '../utils/classnames'
+import * as PropTypes from 'prop-types'
+import {createScopedClasses} from '../utils/classnames'
 import './style'
+import {HTMLAttributes, PureComponent, RefObject} from "react";
 
 const componentName = 'ClickOutside'
 const sc = createScopedClasses(componentName)
-export interface IProps extends IStyledProps {
+
+interface IProps extends HTMLAttributes<HTMLDivElement> {
+    handler?: (e: any) => void
+    exclude?: RefObject<Element>
 }
 
-const ClickOutside: React.FunctionComponent<IProps> = props => {
-  const {  className, style, children } = props
-  return (
-    <div data-role={componentName} style={style} className={classes(sc(),className)}>
-      {children}
-    </div>
-  )
+class ClickOutside extends PureComponent<IProps> {
+    static displayName = componentName
+    static propTypes = {
+        handler: PropTypes.func
+    }
+    private readonly myRef: RefObject<HTMLDivElement>
+
+    constructor(props: IProps) {
+        super(props)
+        this.myRef = React.createRef()
+    }
+
+    handler = (e: MouseEvent | TouchEvent) => {
+        console.log(this.myRef)
+        if (this.myRef.current === null) {
+            return
+        }
+        if (this.props.exclude && this.props.exclude.current && this.props.exclude.current.contains(e.target as Node)) {
+            return
+        }
+        if (!document.contains(e.target as Node)) {
+            return
+        }
+        if (!this.myRef.current.contains(e.target as Node)) {
+            this.props.handler && this.props.handler.call(e.target, e)
+        }
+
+    };
+
+    componentDidMount(): void {
+        this.props.handler && document.addEventListener('click', this.handler);
+    }
+
+    componentWillUnmount(): void {
+        this.props.handler && document.removeEventListener('click', this.handler)
+    }
+
+    render() {
+        const {children, exclude, handler, ...restProps} = this.props
+        return (
+            <div data-role={componentName} className={sc('dd')} ref={this.myRef} {...restProps}>
+                {children}
+            </div>
+        )
+    }
 }
 
-ClickOutside.displayName = componentName
-ClickOutside.defaultProps = {
-}
 
 export default ClickOutside
