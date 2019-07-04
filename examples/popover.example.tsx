e873b1bc2db753f11df5380fc2379f87e7e215e1
FredaFei <964959033@qq.com>
2019-07-04 11:00:39 +0800

    更新 popover 组件示例

--- examples/popover.example.tsx
+++ examples/popover.example.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react'
-import {Popover,Button} from '../packages/index'
+import {Popover, ClickOutside, Button} from '../packages/index'
+import {useRef, useState} from "react";
 
 export default function (props: any) {
-    // const [open, setOpen] = useState(false);
-    // const div = useRef<HTMLDivElement>(null);
+    const [open, setOpen] = useState(false);
+    const div = useRef<HTMLDivElement>(null);
+    const onClick = () => {
+        setOpen(!open)
+    }
+    const onClickOutside = () => {
+        setOpen(false)
+    }
     const content = (
         <div className="example-popover-content" style={{padding: '20px'}}>
-            <div>this is a title</div>
-            <div>
+            <div style={{paddingBottom: '6px', borderBottom: '1px solid #ddd'}}>this is a title</div>
+            <div style={{paddingTop: '6px'}}>
                 <h5>this is a content 1</h5>
                 <h5>this is a content 2</h5>
                 <h5>this is a content 3</h5>
@@ -18,7 +25,7 @@ export default function (props: any) {
     return (
         <div className="exp-box">
             <div className="exp-section">
-                <h3>基础应用</h3>
+                <h3>点击触发</h3>
                 <Popover content={content} position="left">
                     <Button>left</Button>
                 </Popover>
@@ -40,6 +47,9 @@ export default function (props: any) {
                 <Popover content={content} position="bottomLeft">
                     <Button>bottomLeft</Button>
                 </Popover>
+                <Popover content={content} position="bottomRight">
+                    <Button>bottomRight</Button>
+                </Popover>
                 <Popover content={content} position="leftTop">
                     <Button>leftTop</Button>
                 </Popover>
@@ -54,10 +64,26 @@ export default function (props: any) {
                 </Popover>
             </div>
             <div className="exp-section">
-                <h3>日期默认为空</h3>
-                <Popover content={<div>hello ha</div>} >
-                    <Button>click me</Button>
+                <h3>鼠标hover 触发</h3>
+                <Popover content={<div style={{padding: '4px'}}>hello popover</div>} trigger="hover">
+                    <Button>hover me</Button>
                 </Popover>
+            </div>
+            <div className="exp-section">
+                <h3>手动触发</h3>
+                <ClickOutside handler={onClickOutside}>
+                    <Popover content={content} trigger="manual" open={open}>
+                        <Button onClick={onClick}>click me</Button>
+                    </Popover>
+                </ClickOutside>
+            </div>
+            <div className="exp-section">
+                <h3>自定义 container</h3>
+                <div ref={div} style={{position: 'relative',top: '10px'}} className="popover-demo-row">
+                    <Popover content="hello" container={div}>
+                        <Button>hover me</Button>
+                    </Popover>
+                </div>
             </div>
         </div>
     )
