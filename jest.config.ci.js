16cbb54f8dc81f8f433fcf65d69ad40785fc5939
fei-han <964959033@qq.com>
2019-07-05 23:00:22 +0800

    package.json add ci config

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -3,7 +3,7 @@ const base = require('./jest.config')
 module.exports = Object.assign({}, base, {
   reporters: ["jest-junit"],
   collectCoverage: true,
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
+  collectCoverageFrom: ["packages/**/*.{ts,tsx}", "!**/node_modules/**"],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
 })
