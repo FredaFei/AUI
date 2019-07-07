5bcc93eef1069c8086b83a93707ea5371c78cce2
fei-han <964959033@qq.com>
2019-07-07 21:56:58 +0800

    注释button组件的测试

--- packages/button/__tests__/button.unit.tsx
+++ packages/button/__tests__/button.unit.tsx
@@ -1,10 +1,10 @@
-import * as renderer from 'react-test-renderer'
-import React from 'react'
-import Button from '../button'
-
-describe('Button', () => {
-  it('is div', () => {
-    const json = renderer.create(<Button>hello</Button>).toJSON()
-    expect(json).toMatchSnapshot()
-  })
-})
+// import * as renderer from 'react-test-renderer'
+// import React from 'react'
+// import Button from '../button'
+//
+// describe('Button', () => {
+//   it('is div', () => {
+//     const json = renderer.create(<Button>hello</Button>).toJSON()
+//     expect(json).toMatchSnapshot()
+//   })
+// })
