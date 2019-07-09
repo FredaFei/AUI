006145351af02990842a61faf511cde139e5dad1
FredaFei <964959033@qq.com>
2019-07-09 15:24:17 +0800

    格式化 clickOutside 组件

--- packages/clickOutside/clickOutside.tsx
+++ packages/clickOutside/clickOutside.tsx
@@ -8,55 +8,54 @@ const componentName = 'ClickOutside'
 const sc = createScopedClasses(componentName)
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
-    handler?: (e: any) => void
-    exclude?: RefObject<Element>
+  handler?: (e: any) => void
+  exclude?: RefObject<Element>
 }
 
 class ClickOutside extends PureComponent<IProps> {
-    static displayName = componentName
-    static propTypes = {
-        handler: PropTypes.func
+  static displayName = componentName
+  static propTypes = {
+    handler: PropTypes.func
+  }
+  private readonly myRef: RefObject<HTMLDivElement>
+
+  constructor(props: IProps) {
+    super(props)
+    this.myRef = React.createRef()
+  }
+
+  handler = (e: MouseEvent | TouchEvent) => {
+    if (this.myRef.current === null) {
+      return
     }
-    private readonly myRef: RefObject<HTMLDivElement>
-
-    constructor(props: IProps) {
-        super(props)
-        this.myRef = React.createRef()
+    if (this.props.exclude && this.props.exclude.current && this.props.exclude.current.contains(e.target as Node)) {
+      return
     }
-
-    handler = (e: MouseEvent | TouchEvent) => {
-        console.log(this.myRef)
-        if (this.myRef.current === null) {
-            return
-        }
-        if (this.props.exclude && this.props.exclude.current && this.props.exclude.current.contains(e.target as Node)) {
-            return
-        }
-        if (!document.contains(e.target as Node)) {
-            return
-        }
-        if (!this.myRef.current.contains(e.target as Node)) {
-            this.props.handler && this.props.handler.call(e.target, e)
-        }
-
-    };
-
-    componentDidMount(): void {
-        this.props.handler && document.addEventListener('click', this.handler);
+    if (!document.contains(e.target as Node)) {
+      return
+    }
+    if (!this.myRef.current.contains(e.target as Node)) {
+      this.props.handler && this.props.handler.call(e.target, e)
     }
 
-    componentWillUnmount(): void {
-        this.props.handler && document.removeEventListener('click', this.handler)
-    }
+  };
 
-    render() {
-        const {children, exclude, handler, ...restProps} = this.props
-        return (
-            <div data-role={componentName} className={sc('dd')} ref={this.myRef} {...restProps}>
-                {children}
-            </div>
-        )
-    }
+  componentDidMount(): void {
+    this.props.handler && document.addEventListener('click', this.handler);
+  }
+
+  componentWillUnmount(): void {
+    this.props.handler && document.removeEventListener('click', this.handler)
+  }
+
+  render() {
+    const {children, exclude, handler, ...restProps} = this.props
+    return (
+      <div data-role={componentName} className={sc('dd')} ref={this.myRef} {...restProps}>
+        {children}
+      </div>
+    )
+  }
 }
 
 
