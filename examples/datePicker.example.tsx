bf2dd3fe7a565d1f896e651a4a1e8a18d9a8a28e
FredaFei <964959033@qq.com>
2019-07-09 17:30:51 +0800

    更新 datePicker 组件示例

--- examples/datePicker.example.tsx
+++ examples/datePicker.example.tsx
@@ -2,23 +2,26 @@ import * as React from 'react'
 import {DatePicker} from '../packages/index'
 import {useState} from "react";
 
-
 export default function (props: any) {
-
-    const [d, setD] = useState<Date|string>(new Date());
-    const onChange = (date: Date|string) => {
-        console.log(date)
-    }
-    return (
-        <div className="exp-box">
-            <div className="exp-section">
-                <h3>基础应用</h3>
-                <DatePicker value={d} onChange={(day:Date|string) => setD(day)}/>
-            </div>
-            <div className="exp-section">
-                <h3>日期默认为空</h3>
-                <DatePicker onChange={onChange}/>
-            </div>
-        </div>
-    )
+  const [d1, setD1] = useState<Date | string>(new Date());
+  const [d2, setD2] = useState<Date | string>(new Date());
+  const onChange = (date: Date | string) => {
+    console.log(date)
+  }
+  return (
+    <div className="exp-box">
+      <div className="exp-section">
+        <h3>基础应用</h3>
+        <DatePicker value={d1} onChange={(day: Date | string) => setD1(day)}/>
+      </div>
+      <div className="exp-section">
+        <h3>日期默认为空</h3>
+        <DatePicker onChange={onChange}/>
+      </div>
+      <div className="exp-section">
+        <h3>自定义日期星期</h3>
+        <DatePicker firstDayOfWeek={0} value={d2} onChange={(day: Date | string) => setD2(day)}/>
+      </div>
+    </div>
+  )
 }
