202a062a4708d1b28df3a9a1dd3612fc0f0c42e0
FredaFei <964959033@qq.com>
2019-07-11 17:38:25 +0800

    更新 Button 测试用例

--- packages/button/__tests__/button.unit.tsx
+++ packages/button/__tests__/button.unit.tsx
@@ -1,10 +1,41 @@
 import * as renderer from 'react-test-renderer'
 import React from 'react'
+import {configure, render, shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({
+  adapter: new Adapter(),
+});
 import Button from '../button'
 
 describe('Button', () => {
-  it('is div', () => {
+  it('Button 存在', () => {
     const json = renderer.create(<Button>hello</Button>).toJSON()
     expect(json).toMatchSnapshot()
   })
+  it('Button 带 icon,iconPosition默认left', () => {
+    const wrapper = render(<Button icon="settings">设置</Button>)
+    console.log(wrapper)
+    // expect(wrapper.props().icon).toBe('settings')
+    // expect(wrapper.props().iconPosition).toBe('left')
+  })
+  it('Button 带 icon,iconPosition可设置', () => {
+    const wrapper = shallow(<Button iconPosition="right">设置</Button>)
+    console.log(wrapper)
+    expect(wrapper.props().iconPosition).toBe('right')
+  })
+  it('Button 设置 onClick', () => {
+    const mockCallBack = jest.fn();
+    const wrapper = shallow(<Button onClick={mockCallBack}>Ok!</Button>);
+    wrapper.simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+  it('Button 设置 自定义类名', () => {
+    const wrapper = shallow(<Button className="test">设置</Button>)
+    expect(wrapper.find('button').hasClass('test')).toBe(true)
+  })
+  it('Button 设置 自定义样式', () => {
+    const wrapper = shallow(<Button style={{color: 'red'}}>设置</Button>)
+    expect(wrapper.render()).toMatchSnapshot()
+  });
 })
