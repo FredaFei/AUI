a43a54041fbb78be530b12ff235dab90f45721cd
FredaFei <964959033@qq.com>
2019-07-12 13:05:06 +0800

    更新 Button 使用示例

--- examples/button.example.tsx
+++ examples/button.example.tsx
@@ -22,6 +22,10 @@ export default function (props:any) {
           <Button icon="loading"></Button>
           <Button icon="right"></Button>
         </ButtonGroup>
+
+        <ButtonGroup>
+          ha
+        </ButtonGroup>
       </div>
       <div className="exp-section">
         <h3>不可用状态</h3>
