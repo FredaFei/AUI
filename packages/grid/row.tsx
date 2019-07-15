ed3c3b3b0e3e31c10af98e85042e5616f6a89ba9
FredaFei <964959033@qq.com>
2019-07-15 14:46:10 +0800

    函数式改写 Row 组件

--- packages/grid/row.tsx
+++ packages/grid/row.tsx
@@ -1,68 +1,54 @@
 import * as React from 'react'
-import * as PropTypes from 'prop-types'
-import classes, { createScopedClasses } from '../utils/classnames'
-import Col, { IProps as IColProps } from './col'
+import classes, {createScopedClasses} from '../utils/classnames'
+import Col, {IProps as IColProps} from './col'
 import './style'
 
 const componentName = 'Row'
 const sc = createScopedClasses(componentName)
+
 interface IProps extends IStyledProps {
   gutter?: number
   align?: 'left' | 'right' | 'center' | 'spaceBetween' | 'spaceAround'
   verticalAlign?: 'top' | 'center' | 'bottom'
 }
-class Row extends React.Component<IProps> {
-  static displayName = componentName
-  static defaultProps = {
-    gutter: 0,
-    align: 'left',
-    verticalAlign: 'top'
-  }
-  static propTypes = {
-    gutter: PropTypes.number,
-    align: PropTypes.oneOf([
-      'left',
-      'right',
-      'center',
-      'spaceBetween',
-      'spaceAround'
-    ]).isRequired,
-    verticalAlign: PropTypes.oneOf(['top', 'center', 'bottom']).isRequired
-  }
-  constructor(props: IProps) {
-    super(props)
-  }
-  render() {
-    const { style, gutter, align, verticalAlign, className } = this.props
-    const styles = Object.assign(
-      {},
-      {
-        marginLeft: `${-gutter! / 2}px`,
-        marginRight: `${-gutter! / 2}px`,
-        ...style
-      }
-    )
-    const rowWrapClass = classes(
-      sc(
-        '',
-        align !== 'left' && `align-${align}`,
-        verticalAlign !== 'top' && `verticalAlign-${verticalAlign}`
-      ),
-      className
-    )
-    const col = React.Children.map(this.props.children, child => {
-      const element = child as React.ReactElement<IColProps | {}>
-      return element.type === Col
-        ? React.cloneElement(element, {
-            gutter
-          })
-        : element
-    }).filter(i => i)
-    return (
-      <div data-role={componentName} style={styles} className={rowWrapClass}>
-        {col}
-      </div>
-    )
-  }
+
+const Row: React.FunctionComponent<IProps> = props => {
+  const {style, gutter, align, verticalAlign, className} = props
+  const styles = Object.assign(
+    {
+      marginLeft: `${-gutter! / 2}px`,
+      marginRight: `${-gutter! / 2}px`,
+      ...style
+    }
+  )
+  const rowWrapClass = classes(
+    sc(
+      '',
+      align !== 'left' && `align-${align}`,
+      verticalAlign !== 'top' && `verticalAlign-${verticalAlign}`
+    ),
+    className
+  )
+  const col = React.Children.map(props.children, child => {
+    const element = child as React.ReactElement<IColProps | {}>
+    return element.type === Col
+      ? React.cloneElement(element, {
+        gutter
+      })
+      : element
+  }).filter(i => i)
+  return (
+    <div data-role={componentName} style={styles} className={rowWrapClass}>
+      {col}
+    </div>
+  )
 }
+
+Row.displayName = componentName
+Row.defaultProps = {
+  gutter: 0,
+  align: 'left',
+  verticalAlign: 'top'
+}
+
 export default Row
