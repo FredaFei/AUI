8fc86ff58724539dac3cb99eea3bdff25523ed6a
FredaFei <964959033@qq.com>
2019-07-15 17:34:13 +0800

    添加 Switch 组件单元测试

---
+++ packages/switch/__tests__/switch.unit.tsx
@@ -0,0 +1,49 @@
+import * as renderer from 'react-test-renderer'
+import React from 'react'
+import {shallow, mount} from 'enzyme'
+
+import Switch from '../switch'
+import Button from "../../button/button";
+
+describe('测试 Switch 以下行为', () => {
+  it('Switch 存在', () => {
+    const json = renderer.create(<Switch defaultChecked/>).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+  it('Switch 设置 defaultChecked', () => {
+    const wrapper = mount(<Switch defaultChecked/>)
+    expect(wrapper.props().defaultChecked).toEqual(true)
+    expect(wrapper.find('.am-switch').hasClass('am-switch-active')).toEqual(true)
+  })
+  it('Switch 设置 checked', () => {
+    const checked = true
+    const wrapper = mount(<Switch checked={checked}/>)
+    expect(wrapper.props().checked).toEqual(true)
+    expect(wrapper.find('.am-switch').hasClass('am-switch-active')).toEqual(true)
+  })
+  it('Switch 手动触发 onChange', () => {
+    const checked = true
+    const onToggleSwitch = jest.fn();
+    const wrapper = mount(
+      <div>
+        <Switch checked={checked}/>
+        <Button onClick={onToggleSwitch}>toggle</Button>
+      </div>
+    )
+    wrapper.find('.am-button').simulate('click');
+    expect(onToggleSwitch.mock.calls.length).toEqual(1);
+  })
+  it('Switch 设置 disabled', () => {
+    const wrapper = mount(<Switch disabled/>)
+    expect(wrapper.props().disabled).toEqual(true)
+    expect(wrapper.find('.am-switch').hasClass('am-switch-disabled')).toEqual(true)
+  })
+  it('Switch 设置 自定义类名', () => {
+    const wrapper = shallow(<Switch defaultChecked className="test"/>)
+    expect(wrapper.find('.am-switch').hasClass('test')).toBe(true)
+  })
+  it('Switch 设置 自定义样式', () => {
+    const wrapper = shallow(<Switch defaultChecked style={{marginRight: '30px'}}/>)
+    expect(wrapper.render()).toMatchSnapshot()
+  });
+})
