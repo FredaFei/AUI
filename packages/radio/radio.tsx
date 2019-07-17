c95d059f76dfa9282f543b42ceeb3063e53375f8
FredaFei <964959033@qq.com>
2019-07-17 17:30:55 +0800

    更新 Radio 组件

--- packages/radio/radio.tsx
+++ packages/radio/radio.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react'
-import classes, { createScopedClasses } from '../utils/classnames'
+import classes, {createScopedClasses} from '../utils/classnames'
 import './style'
 
 const componentName = 'Radio'
 const sc = createScopedClasses(componentName)
-export interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  disabled?: boolean
+  checked?: boolean
+}
+
 const Radio: React.FunctionComponent<IProps> = props => {
-  const { disabled, checked, children, className, ...rest } = props
+  const {disabled, checked, children, className, ...rest} = props
   return (
-    <label className={classes(sc('wrapper', { disabled, checked }), className)}>
+    <label className={classes(sc('wrapper', {disabled, checked}), className)}>
       <span className={sc('input')}>
         <input type="radio" className={sc('')} checked={checked} {...rest} />
       </span>
@@ -17,4 +22,7 @@ const Radio: React.FunctionComponent<IProps> = props => {
   )
 }
 Radio.displayName = componentName
+Radio.defaultProps = {
+  checked: false
+}
 export default Radio
