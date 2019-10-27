968506d1da07e32e41961e3913549753bbe6bd7d
fredafei <964959033@qq.com>
2019-10-27 17:55:42 +0800

    更新 scroll 组件

--- examples/scroll.example.tsx
+++ examples/scroll.example.tsx
@@ -7,7 +7,40 @@ export default function (props:any) {
     <div className="exp-box">
       <div className="exp-section">
         <h3>基础应用</h3>
-        <Scroll></Scroll>
+        <Scroll style={{height: '300px'}}>
+          <ul>
+            <li>hello, scroll 1</li>
+            <li>hello, scroll 2</li>
+            <li>hello, scroll 3</li>
+            <li>hello, scroll 4</li>
+            <li>hello, scroll 5</li>
+            <li>hello, scroll 6</li>
+            <li>hello, scroll 7</li>
+            <li>hello, scroll 8</li>
+            <li>hello, scroll 9</li>
+            <li>hello, scroll 10</li>
+            <li>hello, scroll 11</li>
+            <li>hello, scroll 12</li>
+            <li>hello, scroll 13</li>
+            <li>hello, scroll 14</li>
+            <li>hello, scroll 15</li>
+            <li>hello, scroll 16</li>
+            <li>hello, scroll 17</li>
+            <li>hello, scroll 18</li>
+            <li>hello, scroll 19</li>
+            <li>hello, scroll 20</li>
+            <li>hello, scroll 21</li>
+            <li>hello, scroll 22</li>
+            <li>hello, scroll 23</li>
+            <li>hello, scroll 24</li>
+            <li>hello, scroll 25</li>
+            <li>hello, scroll 26</li>
+            <li>hello, scroll 27</li>
+            <li>hello, scroll 28</li>
+            <li>hello, scroll 29</li>
+            <li>hello, scroll 30</li>
+          </ul>
+        </Scroll>
       </div>
 
     </div>
