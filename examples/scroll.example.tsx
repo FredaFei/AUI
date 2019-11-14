102c69f4d145c18cb81e7b6a3e000371b2ea7ccd
FredaFei <964959033@qq.com>
2019-11-14 18:05:29 +0800

    更新 scroll 组件示例

--- examples/scroll.example.tsx
+++ examples/scroll.example.tsx
@@ -1,45 +1,28 @@
 import * as React from 'react'
-import {Scroll} from '../packages/index'
+import { Scroll } from '../packages/index'
+import { useState } from "react";
+import { range } from '../packages/utils/collection'
 
-
-export default function (props:any) {
+const page = 15
+let index = 1
+export default function (props: any) {
+  const [source, setSource] = useState(range(1, index * page))
+  const onPullDown = () => {
+    console.log('on down refresh')
+    index = 1
+    setSource(range(1, index * page))
+  }
+  const onPullUp = () => {
+    console.log('on up load more')
+    index += 1
+    setSource(range(1, index * page))
+  }
   return (
     <div className="exp-box">
       <div className="exp-section">
         <h3>基础应用</h3>
-        <Scroll style={{height: '300px'}}>
-          <ul>
-            <li>hello, scroll 1</li>
-            <li>hello, scroll 2</li>
-            <li>hello, scroll 3</li>
-            <li>hello, scroll 4</li>
-            <li>hello, scroll 5</li>
-            <li>hello, scroll 6</li>
-            <li>hello, scroll 7</li>
-            <li>hello, scroll 8</li>
-            <li>hello, scroll 9</li>
-            <li>hello, scroll 10</li>
-            <li>hello, scroll 11</li>
-            <li>hello, scroll 12</li>
-            <li>hello, scroll 13</li>
-            <li>hello, scroll 14</li>
-            <li>hello, scroll 15</li>
-            <li>hello, scroll 16</li>
-            <li>hello, scroll 17</li>
-            <li>hello, scroll 18</li>
-            <li>hello, scroll 19</li>
-            <li>hello, scroll 20</li>
-            <li>hello, scroll 21</li>
-            <li>hello, scroll 22</li>
-            <li>hello, scroll 23</li>
-            <li>hello, scroll 24</li>
-            <li>hello, scroll 25</li>
-            <li>hello, scroll 26</li>
-            <li>hello, scroll 27</li>
-            <li>hello, scroll 28</li>
-            <li>hello, scroll 29</li>
-            <li>hello, scroll 30</li>
-          </ul>
+        <Scroll style={{ height: '300px' }} onPullDown={onPullDown} onPullUp={onPullUp}>
+          <ul>{source.map(i => <li key={i}>hello, scroll {i}</li>)}</ul>
         </Scroll>
       </div>
 
