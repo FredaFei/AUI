1847c21278cd5baf6c10aa9abdda0a860308b8f4
FredaFei <964959033@qq.com>
2019-11-19 10:57:34 +0800

    更新 pager 组件示例

--- examples/pager.example.tsx
+++ examples/pager.example.tsx
@@ -9,23 +9,12 @@ export default function(props: any) {
     <div className="exp-box">
       <div className="exp-section">
         <h3>基础应用</h3>
-        <Pager
-          defaultCurrent={4}
-          total={5}
-          onChange={page => {
-            console.log(page)
-          }}
-        />
+        <Pager defaultCurrent={4} total={5} onChange={page => {console.log(page)}}/>
         <Pager current={page1} total={11} onChange={page => setPage1(page)} />
       </div>
       <div className="exp-section">
         <h3>可跳转</h3>
-        <Pager
-          visibleQuickJumper
-          current={page2}
-          total={10}
-          onChange={page => setPage2(page)}
-        />
+        <Pager visibleQuickJumper current={page2} total={10} onChange={page => setPage2(page)}/>
       </div>
     </div>
   )
