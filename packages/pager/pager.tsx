d3458f1ba70a7ccecd8d44f5882edcb1dc68e83c
FredaFei <964959033@qq.com>
2019-11-19 10:56:47 +0800

    更新 pager 组件, hooks 替代 class

--- packages/pager/pager.tsx
+++ packages/pager/pager.tsx
@@ -1,71 +1,58 @@
 import * as React from 'react'
-import * as PropTypes from 'prop-types'
-import classes, { createScopedClasses } from '../utils/classnames'
-import { range } from '../utils/collection'
+import classes, {createScopedClasses} from '../utils/classnames'
+import {range} from '../utils/collection'
 import Icon from '../icon/icon'
 import Input from '../input/input'
 import './style'
+import {useEffect, useRef, useState} from "react";
 
 const componentName = 'Pager'
 const sc = createScopedClasses(componentName)
-interface IProps extends IStyledProps {
+
+interface Props extends IStyledProps {
   current?: number
   defaultCurrent?: number
   total: number
   visibleQuickJumper?: boolean
   onChange?: (page: number) => any
 }
-interface IState {
-  current?: number
-  value?: string
-}
-class Pager extends React.Component<IProps, IState> {
-  static displayName = componentName
-  static defaultProps = {}
-  state = { current: this.props.defaultCurrent || 1,value:'' }
-  static propTypes = {
-    current: PropTypes.number,
-    total: PropTypes.number,
-    defaultCurrent: PropTypes.number
-  }
-  onClickItem(page: number) {
-    if (page <= this.props.total && page >= 1) {
-      this.current = page
+
+const Pager: React.FunctionComponent<Props> = props => {
+  const {visibleQuickJumper, className, style} = props
+  const [index, setIndex] = useState<number>(1)
+  const initializingRef = useRef<boolean>(true)
+
+  const onClickItem = (page: number) => {
+    if (page <= props.total && page >= 1) {
+      initializingRef.current = false
+      setIndex(page)
+      props.onChange && props.onChange(page)
     }
   }
-  onInputEnter(e: React.KeyboardEvent<HTMLInputElement>): any {
+  const onInputEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const input = e.target as HTMLInputElement
     const page = input.value
     input.value = ''
     if (/^\d+$/.test(page)) {
-      this.current =
-        parseInt(page) > this.props.total ? this.props.total : parseInt(page)
+      initializingRef.current = false
+      setIndex(parseInt(page) > props.total ? props.total : parseInt(page))
     }
   }
-  get current() {
-    if ('defaultCurrent' in this.props) {
-      return this.state.current
-    } else {
-      return this.props.current || 1
-    }
-  }
-  set current(value: number) {
-    if ('defaultCurrent' in this.props) {
-      this.setState({ current: value })
-    }
-    this.props.onChange && this.props.onChange(value)
-  }
-  jumpPages = (index: number): number => {
-    const prev = this.current - 5 <= 0 ? 1 : this.current - 5
-    const next =
-      this.current + 5 >= this.props.total ? this.props.total : this.current + 5
 
+  useEffect(() => {
+    if (!initializingRef.current) {return}
+    const page = 'defaultCurrent' in props ? props.defaultCurrent : props.current ? props.current : 1
+    setIndex(page as number)
+
+  })
+  const jumpPages = (index: number): number => {
+    const prev = index - 5 <= 0 ? 1 : index - 5
+    const next = index + 5 >= props.total ? props.total : index + 5
     return index === 1 ? prev : next
   }
-  get items() {
-    const { total } = this.props
-    return range(1, total)
-      .filter(i => i === 1 || i === total || Math.abs(i - this.current) <= 2)
+  const items = () => {
+    return range(1, props.total)
+      .filter(i => i === 1 || i === props.total || Math.abs(i - index) <= 2)
       .reduce(
         (prev, next) => {
           const last = prev[prev.length - 1]
@@ -74,59 +61,30 @@ class Pager extends React.Component<IProps, IState> {
         },
         [] as number[]
       )
-      .map((item, index) =>
-        item === -1 ? (
-          <li
-            key={`pager-split-${index}`}
-            className={sc('list-item', 'split')}
-            onClick={this.onClickItem.bind(this, this.jumpPages(index))}
-          >
-            <span>...</span>
-          </li>
-        ) : (
-          <li
-            key={`pager-item-${index}`}
-            className={sc('list-item', {
-              'list-item-active': this.current === item
-            })}
-            onClick={this.onClickItem.bind(this, item)}
-          >
-            {item}
-          </li>
-        )
+      .map((item, i) => item === -1 ?
+          (<li key={`pager-split-${i}`} className={sc('list-item', 'split')} onClick={() => onClickItem(jumpPages(i))}><span>...</span></li>)
+            : (<li key={`pager-item-${i}`} className={sc('list-item', {'list-item-active': index === item})} onClick={() => onClickItem(item)}>{item}</li>)
       )
   }
-  render() {
-    const { current, total, visibleQuickJumper, className, style } = this.props
-    return (
-      <div className={classes(sc('wrapper'), className)} style={style}>
-        <span
-          className={sc('prev', { disabled: current === 1 })}
-          onClick={this.onClickItem.bind(this, current! - 1)}
-        >
-          <Icon name="left" />
+
+  return (
+    <div className={classes(sc('wrapper'), className)} style={style}>
+      <span className={sc('prev', {disabled: index === 1})} onClick={() => onClickItem(index - 1)}>
+        <Icon name="left"/>
+      </span>
+      <ul className={sc('list')}>{items()}</ul>
+      <span className={sc('next', {disabled: index === props.total})} onClick={() => onClickItem(index + 1)}>
+        <Icon name="right"/>
+      </span>
+      {visibleQuickJumper && (
+        <span className={sc('quick-jump')}>跳至
+          <Input className={sc('input-page')} type="text" onEnter={onInputEnter}/>页
         </span>
-        <ul className={sc('list')}>{this.items}</ul>
-        <span
-          className={sc('next', { disabled: current === total })}
-          onClick={this.onClickItem.bind(this, current! + 1)}
-        >
-          <Icon name="right" />
-        </span>
-        {visibleQuickJumper && (
-          <span className={sc('quick-jump')}>
-            跳至
-            <Input
-              className={sc('input-page')}
-              type="text"
-              onEnter={this.onInputEnter.bind(this)}
-            />
-            页
-          </span>
-        )}
-      </div>
-    )
-  }
+      )}
+    </div>
+  )
 }
 
+Pager.displayName = componentName
+Pager.defaultProps = {}
 export default Pager
