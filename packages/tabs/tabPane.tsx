8fed167686c008e36d6add9c95b9a037cb181e59
FredaFei <964959033@qq.com>
2019-11-20 11:03:39 +0800

    更新 tabs 组件, hooks 替代 class

--- packages/tabs/tabPane.tsx
+++ packages/tabs/tabPane.tsx
@@ -1,38 +1,27 @@
 import * as React from 'react'
-import * as PropTypes from 'prop-types'
 import classes, { createScopedClasses } from '../utils/classnames'
 import './style'
 
 const componentName = 'TabPane'
 const sc = createScopedClasses(componentName)
-export interface IProps extends IStyledProps {
+
+export interface Props extends IStyledProps {
   tab: React.ReactNode
   key?: string
   active?: boolean
   disabled?: boolean
 }
-class TabPane extends React.Component<IProps> {
-  static displayName = componentName
-  static defaultProps = {}
-  static propTypes = {
-    tab: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
-    active: PropTypes.bool,
-    disabled: PropTypes.bool
-  }
-  render() {
-    const { className, style, active, key, children, ...rest } = this.props
-    const paneClasses = classes(sc('pane-item', { active }), className)
-    return (
-      <li
-        data-role="tabPane"
-        key={key}
-        className={paneClasses}
-        style={style}
-        {...rest}
-      >
-        {children}
-      </li>
-    )
-  }
+
+const TabPane:React.FunctionComponent<Props>=props=>{
+  const { className, active, key, children, ...rest } = props
+  return (
+    <li data-role={componentName} key={key} className={classes(sc('pane-item', { active }), className)} {...rest}>
+      {children}
+    </li>
+  )
 }
+
+TabPane.displayName = componentName
+TabPane.defaultProps = {}
+
 export default TabPane
--- packages/tabs/tabs.tsx
+++ packages/tabs/tabs.tsx
@@ -1,80 +1,59 @@
 import * as React from 'react'
-import * as PropTypes from 'prop-types'
-import classes, { createScopedClasses } from '../utils/classnames'
-import TabPane, { IProps as IPaneProps } from './tabPane'
+import classes, {createScopedClasses} from '../utils/classnames'
+import TabPane, {Props as PaneProps} from './tabPane'
 import './style'
+import {useEffect, useRef, useState} from "react";
 
 const componentName = 'Tabs'
 const sc = createScopedClasses(componentName)
-interface IProps extends IStyledProps {
+
+export interface Props extends IStyledProps{
   activeKey?: string
   defaultActiveKey?: string
   direction?: string
   onChange?: (key: string) => any
 }
-interface ILayout {
+
+interface Layout {
   key: string
   active?: boolean
 }
 
-type TLayout = (a: React.ReactElement<IPaneProps>, b: ILayout) => {}
-interface IState {
-  activeTabKey: string
-}
-class Tabs extends React.Component<IProps, IState> {
-  static displayName = componentName
-  static defaultProps = {
-    direction: 'horizontal'
+type TLayout = (a: React.ReactElement<PaneProps>, b: Layout) => {}
+
+const Tabs: React.FunctionComponent<Props> = props => {
+  const [activeTabKey, setActiveTabKey] = useState<string>('')
+
+  const {className, direction, defaultActiveKey, activeKey,style} = props
+
+  const keysRef = useRef<Array<string>>([])
+  const lineRef = useRef<HTMLDivElement>(null)
+  const tabsHeadRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const active = 'defaultActiveKey' in props ? defaultActiveKey : 'activeKey' in props ? activeKey : (keysRef.current[0] || '')
+    setActiveTabKey(active as string)
+    calculateLineStyle(getCurrentTabsIndex(active as string))
+  }, [])
+
+  const getCurrentTabsIndex = (value: string): number => {
+    const index = keysRef.current.indexOf(value)
+    return index >= 0 ? index : 0
   }
-  static propTypes = {
-    activeKey: PropTypes.string,
-    defaultActiveKey: PropTypes.string,
-    direction: PropTypes.string
-  }
-   lineElement: React.RefObject<HTMLDivElement>
-   tabsHeadElement: React.RefObject<HTMLDivElement>
-  constructor(props: IProps) {
-    super(props)
-    this.lineElement = React.createRef()
-    this.tabsHeadElement = React.createRef()
-    this.state = {
-      activeTabKey:
-        this.props.defaultActiveKey || this.props.activeKey || this._keys[0]
-    }
-  }
-  componentDidMount() {
-    if(!this.state.activeTabKey){
-      this.setState({
-        activeTabKey:
-          this.props.defaultActiveKey || this.props.activeKey || this._keys[0]
-      })
-    }
-    this.calculateLineStyle(this.getCurrentTabsIndex(this.state.activeTabKey))
-  }
-  componentDidUpdate(nextProps: IProps, prevState: IState) {
-    if (this.state.activeTabKey !== prevState.activeTabKey) {
-      this.calculateLineStyle(
-        this.getCurrentTabsIndex(this.state.activeTabKey)
-      )
-    }
-  }
-  _keys: string[] = []
-  getCurrentTabsIndex = (value: string): number => {
-    return this._keys.indexOf(value) || 0
-  }
-  calculateLineStyle(index: number): any {
+
+  const calculateLineStyle = (index: number): any => {
     if (index < 0) {
       return false
     }
-    const { direction } = this.props
-    const lineElement = this.lineElement.current
-    const tabsHeadElement = this.tabsHeadElement.current
+    const {direction} = props
+    const lineElement = lineRef.current
+    const tabsHeadElement = tabsHeadRef.current
     if (!lineElement || !tabsHeadElement || !tabsHeadElement.children) {
       return false
     }
     const el = tabsHeadElement.children[index]
-    let { left: left1, top: top1 } = tabsHeadElement.getBoundingClientRect()
-    let { width, left: left2, height, top: top2 } = el.getBoundingClientRect()
+    let {left: left1, top: top1} = tabsHeadElement.getBoundingClientRect()
+    let {width, left: left2, height, top: top2} = el.getBoundingClientRect()
     if (direction === 'horizontal') {
       lineElement.style.width = `${width}px`
       lineElement.style.left = `${left2 - left1}px`
@@ -84,17 +63,15 @@ class Tabs extends React.Component<IProps, IState> {
     }
   }
 
-  handleClick = (key: string, disabled: boolean): any => {
+  const handleClick = (key: string, disabled: boolean): any => {
     if (disabled) {
       return false
     }
-    this.setState({
-      activeTabKey: key
-    })
-    this.calculateLineStyle(this.getCurrentTabsIndex(key))
-    this.props.onChange && this.props.onChange(key)
+    setActiveTabKey(key)
+    calculateLineStyle(getCurrentTabsIndex(key))
+    props.onChange && props.onChange(key)
   }
-  renderTabsNav = (child: React.ReactElement, options: ILayout) => {
+  const renderTabsNav = (child: React.ReactElement, options: Layout) => {
     const itemClass = {
       disabled: child.props.disabled,
       active: options.active
@@ -104,46 +81,45 @@ class Tabs extends React.Component<IProps, IState> {
         data-role="tabsNavItem"
         key={`nav-${options.key}`}
         className={sc('nav-item', itemClass)}
-        onClick={() => this.handleClick(options.key, child.props.disabled)}
-      >
-        {child.props.tab}
-      </div>
+        onClick={() => handleClick(options.key, child.props.disabled)}
+      >{child.props.tab}</div>
     )
   }
-  renderTabsPane = (child: React.ReactElement, options: ILayout) => {
+
+  const renderTabsPane = (child: React.ReactElement, options: Layout) => {
     return React.cloneElement(child, {
       active: options.active
     })
   }
-  renderLayout = (layout: TLayout) => {
-    const children = React.Children.map(this.props.children, child => {
-      const element = child as React.ReactElement<IPaneProps>
+  const renderLayout = (layout: TLayout) => {
+    const children = React.Children.map(props.children, child => {
+      const element = child as React.ReactElement<PaneProps>
       if (element.type !== TabPane) {
         console.error('Tabs 组件的子组件只能是 TabPane组件')
         return null
       }
       const key = element.key as string
-      this._keys.push(key)
-      const active = this.state.activeTabKey === key
-      return element.type === TabPane && layout(element, { key, active })
+      keysRef.current.push(key)
+      return element.type === TabPane && layout(element, {key, active: activeTabKey === key})
     })
     return children.filter(i => i)
   }
-  render() {
-    const { className, style, direction } = this.props
-    const vertical = { vertical: direction === 'vertical' }
-    const tabsClasses = classes(sc('wrapper', vertical), className)
-    return (
-      <div data-role="tabs" className={tabsClasses} style={style}>
-        <div className={sc('nav')} ref={this.tabsHeadElement}>
-          {this.renderLayout(this.renderTabsNav)}
-          <div className={sc('nav-line')} ref={this.lineElement} />
-        </div>
-        <ul className="am-tabs-pane-body">
-          {this.renderLayout(this.renderTabsPane)}
-        </ul>
+
+  return (
+    <div data-role={componentName} className={classes(sc('wrapper', {vertical: direction === 'vertical'}), className)} style={style}>
+      <div className={sc('nav')} ref={tabsHeadRef}>
+        {renderLayout(renderTabsNav)}
+        <div className={sc('nav-line')} ref={lineRef}/>
       </div>
-    )
-  }
+      <ul className="am-tabs-pane-body">
+        {renderLayout(renderTabsPane)}
+      </ul>
+    </div>
+  )
+}
+
+Tabs.displayName = componentName
+Tabs.defaultProps = {
+  direction: 'horizontal'
 }
 export default Tabs
