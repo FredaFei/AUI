e894545511c6e775d50ed431047d2fac1259a45a
FredaFei <964959033@qq.com>
2019-12-04 11:05:44 +0800

    添加 Layout 组件示例代码

--- examples/layout.example.tsx
+++ examples/layout.example.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react'
-import { Header, Aside, Main, Footer, Layout } from '../packages/layout/index'
+import {Header, Aside, Main, Footer, Layout} from '../packages/layout/index'
 import CodeBox from "./codeBox";
 import content1 from "./markdown/layout-demo-1.md";
 import content2 from "./markdown/layout-demo-2.md";
 import content3 from "./markdown/layout-demo-3.md";
 import content4 from "./markdown/layout-demo-4.md";
 
-export default function(props: any) {
+export default function (props: any) {
   return (
     <div className="exp-box">
+      <h3>基础应用</h3>
       <div className="exp-section">
-        <h3>基础应用</h3>
         <p>上中下布局</p>
         <Layout className="custom-layout">
           <Header>Header</Header>
           <Main>Content</Main>
           <Footer>Footer</Footer>
         </Layout>
+        <CodeBox content={content1}/>
       </div>
-      <CodeBox content={content1}/>
       <div className="exp-section">
         <p>左右布局-1</p>
         <Layout className="custom-layout">
@@ -30,6 +30,8 @@ export default function(props: any) {
           <Footer>Footer</Footer>
         </Layout>
         <CodeBox content={content2}/>
+      </div>
+      <div className="exp-section">
         <p>左右布局-2</p>
         <Layout className="custom-layout">
           <Header>Header</Header>
@@ -39,8 +41,8 @@ export default function(props: any) {
           </Layout>
           <Footer>Footer</Footer>
         </Layout>
+        <CodeBox content={content3}/>
       </div>
-      <CodeBox content={content3}/>
       <div className="exp-section">
         <p>侧边栏布局</p>
         <Layout className="custom-layout">
@@ -51,8 +53,8 @@ export default function(props: any) {
             <Footer>Footer</Footer>
           </Layout>
         </Layout>
+        <CodeBox content={content4}/>
       </div>
-      <CodeBox content={content4}/>
     </div>
   )
 }
