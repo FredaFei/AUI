c411593fab545464bbab2d7820ca8ac527f1d6f2
FredaFei <964959033@qq.com>
2019-12-05 14:18:53 +0800

    添加 Pager 组件示例文档

---
+++ examples/markdown/pager-doc.md
@@ -0,0 +1,12 @@
+### API
+
+| 参数         |说明        |类型 |默认值 |可选值 |
+| ------------ | :----------------|:------- | :----- | :----- |
+| current         |设置当前页数 |Number |—— |—— |
+| defaultCurrent |设置默认的当前页数 |Number |—— |—— |
+| total |数据总数 |Number |—— |—— |
+| visibleQuickJumper      |是否可以快速跳转至某页  |Boolean |false |true |
+| className      |自定义类名  |string |—— |—— |
+| style      |自定义样式  |React.CSSProperties |—— |—— |
+| onChange    | 页码改变的回调，参数是改变后的页码 |       (page) => void |—— |—— |
+
--- examples/pager.example.tsx
+++ examples/pager.example.tsx
@@ -5,6 +5,8 @@ import Pager from '../packages/pager/pager'
 import content1 from "./markdown/pager-demo-1.md";
 import content2 from "./markdown/pager-demo-2.md";
 import CodeBox from "./codeBox";
+import Markdown from "./markdown";
+import doc from "./markdown/pager-doc.md";
 
 export default function (props: any) {
   const [page1, setPage1] = useState(2)
@@ -22,6 +24,7 @@ export default function (props: any) {
         <Pager visibleQuickJumper current={page2} total={10} onChange={page => setPage2(page)}/>
         <CodeBox content={content2}/>
       </div>
+      <Markdown source={doc}/>
     </div>
   )
 }
