73c460927434cdf46ea9f96399962023e6f29278
FredaFei <964959033@qq.com>
2019-12-06 15:29:16 +0800

    更新文档UI 高亮设置

--- examples/markdown.tsx
+++ examples/markdown.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import ReactMarkdown from 'react-markdown'
+import CodeBlock from "./codeBlock";
 
 export default function (props:any) {
   return (
     <div className="markdown-content">
-      <ReactMarkdown source={props.source} escapeHtml={false}/>
+      <ReactMarkdown source={props.source}
+                     escapeHtml={false}
+                     renderers={{
+                       code: CodeBlock
+                     }}></ReactMarkdown>
     </div>
   )
 }
