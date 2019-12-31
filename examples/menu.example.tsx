4589d7976d8478a715f4f48cfcb2da868d47efcf
fredafei <964959033@qq.com>
2019-12-31 22:34:26 +0800

    更新 Menu 组件示例

--- examples/menu.example.tsx
+++ examples/menu.example.tsx
@@ -48,38 +48,38 @@ export default function (props: any) {
           <MenuItem name="menu4">menu 4</MenuItem>
         </Menu>
       </div>
-      {/*<div className="exp-section">*/}
-      {/*  <h3>多层嵌套</h3>*/}
-      {/*  <Menu selected="menu1">*/}
-      {/*    <MenuItem name="menu1">menu 1</MenuItem>*/}
-      {/*    <MenuItem name="menu2">menu 2</MenuItem>*/}
-      {/*    <SubMenu title="menu mixin 3" name="menu3">*/}
-      {/*      <MenuItem name="menu3-1">menu 3--1</MenuItem>*/}
-      {/*      <MenuItem name="menu3-2">menu 3--2</MenuItem>*/}
-      {/*      <SubMenu title="menu mixin 3" name="menu3-2">*/}
-      {/*        <MenuItem name="menu3-2-1">menu 3-2-1</MenuItem>*/}
-      {/*        <MenuItem name="menu3-2-2">menu 3-2-2</MenuItem>*/}
-      {/*        <MenuItem name="menu3-3-3">menu 3-2-3</MenuItem>*/}
-      {/*      </SubMenu>*/}
-      {/*    </SubMenu>*/}
-      {/*  </Menu>*/}
-      {/*  <CodeBox content={content3}/>*/}
-      {/*</div>*/}
-      {/*<div className="exp-section">*/}
-      {/*  <h3>不可用状态</h3>*/}
-      {/*  <Menu selected="menu1">*/}
-      {/*    <MenuItem name="menu1">menu 1</MenuItem>*/}
-      {/*    <MenuItem name="menu2" disabled>menu 2</MenuItem>*/}
-      {/*    <MenuItem name="menu3">menu 3</MenuItem>*/}
-      {/*    <SubMenu title="menu mixin 4" name="menu4" disabled>*/}
-      {/*      <MenuItem name="menu3-1">menu 3--1</MenuItem>*/}
-      {/*      <MenuItem name="menu3-2">menu 3--2</MenuItem>*/}
-      {/*      <MenuItem name="menu3-3">menu 3--3</MenuItem>*/}
-      {/*    </SubMenu>*/}
-      {/*  </Menu>*/}
-      {/*  <CodeBox content={content4}/>*/}
-      {/*</div>*/}
-      {/*<CodeBox content={content2}/>*/}
+      <div className="exp-section">
+        <h3>多层嵌套</h3>
+        <Menu selected="menu1">
+          <MenuItem name="menu1">menu 1</MenuItem>
+          <MenuItem name="menu2">menu 2</MenuItem>
+          <SubMenu title="menu mixin 3" name="menu3">
+            <MenuItem name="menu3-1">menu 3--1</MenuItem>
+            <MenuItem name="menu3-2">menu 3--2</MenuItem>
+            <SubMenu title="menu mixin 3" name="menu3-2">
+              <MenuItem name="menu3-2-1">menu 3-2-1</MenuItem>
+              <MenuItem name="menu3-2-2">menu 3-2-2</MenuItem>
+              <MenuItem name="menu3-3-3">menu 3-2-3</MenuItem>
+            </SubMenu>
+          </SubMenu>
+        </Menu>
+        <CodeBox content={content3}/>
+      </div>
+      <div className="exp-section">
+        <h3>不可用状态</h3>
+        <Menu selected="menu1">
+          <MenuItem name="menu1">menu 1</MenuItem>
+          <MenuItem name="menu2" disabled>menu 2</MenuItem>
+          <MenuItem name="menu3">menu 3</MenuItem>
+          <SubMenu title="menu mixin 4" name="menu4" disabled>
+            <MenuItem name="menu3-1">menu 3--1</MenuItem>
+            <MenuItem name="menu3-2">menu 3--2</MenuItem>
+            <MenuItem name="menu3-3">menu 3--3</MenuItem>
+          </SubMenu>
+        </Menu>
+        <CodeBox content={content4}/>
+      </div>
+      <CodeBox content={content2}/>
       <Markdown source={doc}/>
     </div>
   )
