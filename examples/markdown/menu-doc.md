9780291e729c64f566a094e68d846e8f2e161df5
FredaFei <964959033@qq.com>
2020-01-03 16:19:30 +0800

    更新 Menu 组件文档

--- examples/markdown/menu-doc.md
+++ examples/markdown/menu-doc.md
@@ -1,11 +1,29 @@
 ### API
+### Menu
 
 | 参数         |说明        |类型 |默认值 |可选值 |
 | ------------ | :----------------|:------- | :----- | :----- |
-| icon         |设置按钮的图标类型 |String |—— |—— |
-| iconPosition |设置图标的摆放位置 |String |left |right |
-| disabled      |设置按钮失效状态  |Boolean |false |true |
+| selected         |选中的菜单项 |String |—— |—— |
+| layout |菜单样式，现在支持垂直、水平 |String |vertical |`horizontal` |
 | className      |自定义类名  |String |—— |—— |
 | style      |自定义样式  |React.CSSProperties |—— |—— |
-| onClick    | 点击按钮时触发 |       (event) => void |—— |—— |
+| onChange    | 点击 MenuItem 时触发 |       (value) => void |—— |—— |
 
+### MenuItem
+
+| 参数         |说明        |类型 |默认值 |可选值 |
+| ------------ | :----------------|:------- | :----- | :----- |
+| name         |唯一标志 |String |—— |—— |
+| disabled |	是否禁用 |Boolean |false |`true` |
+| className      |自定义类名  |String |—— |—— |
+| style      |自定义样式  |React.CSSProperties |—— |—— |
+
+### SubMenu
+
+| 参数         |说明        |类型 |默认值 |可选值 |
+| ------------ | :----------------|:------- | :----- | :----- |
+| name         |唯一标志 |String |—— |—— |
+| title         |子菜单项标题 |String |—— |—— |
+| disabled |	是否禁用 |Boolean |false |`true` |
+| className      |自定义类名  |String |—— |—— |
+| style      |自定义样式  |React.CSSProperties |—— |—— |
--- examples/menu.example.tsx
+++ examples/menu.example.tsx
@@ -4,22 +4,22 @@ import {Menu, MenuItem, SubMenu} from '../packages/index'
 import content1 from "./markdown/menu-demo-1.md";
 import content2 from "./markdown/menu-demo-2.md";
 import content3 from "./markdown/menu-demo-3.md";
-import content4 from "./markdown/menu-demo-4.md";
 import doc from "./markdown/menu-doc.md";
 import Markdown from './markdown'
 import CodeBox from "./codeBox";
 
 export default function (props: any) {
+  const style = {overflow: 'initial'}
   return (
     <div className="exp-box">
-      <div className="exp-section">
+      <div className="exp-section" style={style}>
         <h3>基础应用</h3>
-        <Menu selected="menu3-3">
-          <SubMenu title="submenu 1" name="submenu1">
+        <Menu >
+          <SubMenu title="sub1" name="sub1">
             <MenuItem name="menu3-1">menu 3--1</MenuItem>
             <MenuItem name="menu3-2">menu 3--2</MenuItem>
             <MenuItem name="menu3-3">menu 3--3</MenuItem>
-            <SubMenu title="submenu 2" name="submenu2">
+            <SubMenu title="submenu 2" name="sub2">
               <MenuItem name="menu3-2-1">menu 3-2-1</MenuItem>
               <MenuItem name="menu3-2-2">menu 3-2-2</MenuItem>
               <MenuItem name="menu3-3-3">menu 3-2-3</MenuItem>
@@ -30,7 +30,7 @@ export default function (props: any) {
         </Menu>
         <CodeBox content={content1}/>
       </div>
-      <div className="exp-section">
+      <div className="exp-section" style={style}>
         <h3>垂直排列</h3>
         <Menu layout="vertical" selected="menu3-2-2">
           <MenuItem name="menu1">menu 1</MenuItem>
@@ -49,24 +49,7 @@ export default function (props: any) {
         </Menu>
         <CodeBox content={content2}/>
       </div>
-      <div className="exp-section">
-        <h3>多层嵌套</h3>
-        <Menu selected="menu1">
-          <MenuItem name="menu1">menu 1</MenuItem>
-          <MenuItem name="menu2">menu 2</MenuItem>
-          <SubMenu title="menu mixin 3" name="menu3">
-            <MenuItem name="menu3-1">menu 3--1</MenuItem>
-            <MenuItem name="menu3-2">menu 3--2</MenuItem>
-            <SubMenu title="menu mixin 3" name="menu3-2">
-              <MenuItem name="menu3-2-1">menu 3-2-1</MenuItem>
-              <MenuItem name="menu3-2-2">menu 3-2-2</MenuItem>
-              <MenuItem name="menu3-3-3">menu 3-2-3</MenuItem>
-            </SubMenu>
-          </SubMenu>
-        </Menu>
-        <CodeBox content={content3}/>
-      </div>
-      <div className="exp-section">
+      <div className="exp-section" style={style}>
         <h3>不可用状态</h3>
         <Menu selected="menu1">
           <MenuItem name="menu1">menu 1</MenuItem>
@@ -78,7 +61,7 @@ export default function (props: any) {
             <MenuItem name="menu3-3">menu 3--3</MenuItem>
           </SubMenu>
         </Menu>
-        <CodeBox content={content4}/>
+        <CodeBox content={content3}/>
       </div>
       <CodeBox content={content2}/>
       <Markdown source={doc}/>
