1b0ac36d59a26b079700ef273821e02af1982611
fredafei <964959033@qq.com>
2020-03-16 09:02:31 +0800

    IProps change to Props

--- packages/clickOutside/clickOutside.tsx
+++ packages/clickOutside/clickOutside.tsx
@@ -1,48 +1,48 @@
-import * as React from 'react'
-import {createScopedClasses} from '../utils/classnames'
-import './style'
-import {HTMLAttributes, RefObject, useEffect,useRef} from "react";
+import * as React from 'react';
+import { createScopedClasses } from '../utils/classnames';
+import './style';
+import { HTMLAttributes, RefObject, useEffect, useRef } from 'react';
 
-const componentName = 'ClickOutside'
-const sc = createScopedClasses(componentName)
+const componentName = 'ClickOutside';
+const sc = createScopedClasses(componentName);
 
-interface IProps extends HTMLAttributes<HTMLDivElement> {
+interface Props extends HTMLAttributes<HTMLDivElement> {
   handler?: (e: any) => void
   exclude?: RefObject<Element>
 }
 
-const ClickOutside: React.FunctionComponent<IProps>=props=>{
-  const myRef = useRef<HTMLDivElement>(null)
-  const {children, exclude, handler, ...restProps} = props
+const ClickOutside: React.FunctionComponent<Props> = props => {
+  const myRef = useRef<HTMLDivElement>(null);
+  const {children, exclude, handler, ...restProps} = props;
 
   const onHandler = (e: MouseEvent | TouchEvent) => {
     if (myRef.current === null) {
-      return
+      return;
     }
     if (exclude && exclude.current && exclude.current.contains(e.target as Node)) {
-      return
+      return;
     }
     if (!document.contains(e.target as Node)) {
-      return
+      return;
     }
     if (!myRef.current.contains(e.target as Node)) {
-      handler && handler.call(e.target, e)
+      handler && handler.call(e.target, e);
     }
-  }
-  useEffect(()=>{
+  };
+  useEffect(() => {
     handler && document.addEventListener('click', onHandler);
-    return ()=>{
-      handler && document.removeEventListener('click', onHandler)
-    }
-  },[])
+    return () => {
+      handler && document.removeEventListener('click', onHandler);
+    };
+  }, []);
 
   return (
     <div data-role={componentName} className={sc('')} ref={myRef} {...restProps}>
       {children}
     </div>
-  )
-}
+  );
+};
 
-ClickOutside.displayName = componentName
-ClickOutside.defaultProps = {}
-export default ClickOutside
+ClickOutside.displayName = componentName;
+ClickOutside.defaultProps = {};
+export default ClickOutside;
