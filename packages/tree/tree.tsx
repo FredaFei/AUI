327a52e778b5ff2b183745283342e2a8c71e7d38
fredafei <964959033@qq.com>
2020-03-17 19:51:03 +0800

    更新tree 组件 单选及多选功能

--- packages/tree/tree.tsx
+++ packages/tree/tree.tsx
@@ -12,33 +12,33 @@ export interface SourceItem {
 }
 
 interface Props extends StyledProps {
-  sourceData: SourceItem[],
-  selected: string[],
-  onChange: (item: SourceItem, checked: boolean) => void,
+  sourceData: SourceItem[]
+  selected: string[]
+  multiple?: boolean
+  onChange: (item: SourceItem, checked: boolean) => void
 }
 
 const Tree: React.FunctionComponent<Props> = props => {
-  const renderTreeItem = (item: SourceItem,
-                          selected: string[],
-                          onChange: (item: SourceItem, checked: boolean) => void,
-                          leave = 1) => {
+  const renderTreeItem = (item: SourceItem, leave = 1) => {
     const classes = {
       [`leave-${leave}`]: true,
       'item': true,
     };
     return <div className={sc(classes)} key={item.value}>
-      <input type="checkbox" checked={selected.includes(item.value)} onChange={e => onChange(item, e.target.checked)}/>
+      <input type="checkbox" checked={props.selected.includes(item.value)} onChange={e => props.onChange(item, e.target.checked)}/>
       <div className={sc('text')}>{item.text}</div>
-      {item.children?.map(subItem => renderTreeItem(subItem, selected, onChange, leave + 1))}
+      {item.children?.map(subItem => renderTreeItem(subItem,leave + 1))}
     </div>;
   };
 
   return <div className={classes(sc(''))}>
     {
-      props.sourceData.map(item => renderTreeItem(item, props.selected, props.onChange))
+      props.sourceData.map(item => renderTreeItem(item))
     }
   </div>;
 };
 Tree.displayName = componentName;
-Tree.defaultProps = {};
+Tree.defaultProps = {
+  multiple: true
+};
 export default Tree;
