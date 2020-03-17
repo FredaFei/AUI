8580fc8e938673f47a23a1859c4719adc51295e5
fredafei <964959033@qq.com>
2020-03-17 17:02:44 +0800

    更新 tree 组件，递归实现

--- examples/tree.example.tsx
+++ examples/tree.example.tsx
@@ -1,20 +1,48 @@
-import * as React from 'react'
-import Tree from '../packages/tree/tree'
+import * as React from 'react';
+import Tree,{SourceItem} from '../packages/tree/tree';
 
-import content1 from "./markdown/tree-demo-1.md";
-import CodeBox from "./codeBox";
-import Markdown from "./markdown";
-import doc from "./markdown/tree-doc.md";
+import content1 from './markdown/tree-demo-1.md';
+import CodeBox from './codeBox';
+import Markdown from './markdown';
+import doc from './markdown/tree-doc.md';
+import { useState } from 'react';
 
 export default function (props: any) {
+  const [data, setData] = useState<SourceItem[]>([
+    {
+      text: '1',
+      value: '1',
+      children: [
+        {
+          text: '1.1',
+          value: '1.1',
+        },{
+          text: '1.2',
+          value: '1.2',
+        },
+      ],
+    }, {
+      text: '2',
+      value: '2',
+      children: [
+        {
+          text: '2.1',
+          value: '2.1',
+        },{
+          text: '2.2',
+          value: '2.2',
+        }
+      ],
+    }
+  ]);
   return (
     <div className="exp-box">
       <h3>基础应用</h3>
       <div className="exp-section">
-        <Tree />
+        <Tree sourceData={data}/>
         <CodeBox content={content1}/>
       </div>
       <Markdown source={doc}/>
     </div>
-  )
+  );
 }
--- packages/tree/tree.tsx
+++ packages/tree/tree.tsx
@@ -5,11 +5,27 @@ import './style';
 const componentName = 'Tree';
 const sc = createScopedClasses(componentName);
 
-export interface Props extends StyledProps {
+export interface SourceItem {
+  value: string
+  text: string
+  children?: SourceItem[]
+}
+
+interface Props extends StyledProps {
+  sourceData: SourceItem[]
 }
 
 const Tree: React.FunctionComponent<Props> = props => {
-  return <div className={classes(sc('tree'))}>tree</div>;
+  return <div className={classes(sc('tree'))}>
+    {
+      props.sourceData.map(item => {
+        return <div className={sc('item')} key={item.value}>
+          <div className={sc('text')}>{item.text}</div>
+          {item.children && item.children.map(item2 => <div className={sc('text')} key={item2.value}>{item2.text}</div>)}
+        </div>;
+      })
+    }
+  </div>;
 };
 Tree.displayName = componentName;
 Tree.defaultProps = {};
