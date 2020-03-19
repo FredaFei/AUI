b9f68647356dea6db5bdb6d858200d2a05e1ed9a
fredafei <964959033@qq.com>
2020-03-19 12:22:26 +0800

    添加数组 求差值和去重功能

--- packages/utils/collection.ts
+++ packages/utils/collection.ts
@@ -1,9 +1,18 @@
 function range(start: number, end: number) {
-  const temp: number[] = []
+  const temp: number[] = [];
   for (let i = start; i <= end; i++) {
-    temp.push(i)
+    temp.push(i);
   }
-  return temp
+  return temp;
 }
 
-export { range }
+function unique(array: any[]) {
+  const obj = {};
+  return array.filter(item => obj.hasOwnProperty(typeof item + JSON.stringify(item)) ? false : (obj[typeof item + JSON.stringify(item)] = true));
+}
+
+function difference(a: any[], b: any[]) {
+  return a.filter(i => b.indexOf(i) === -1);
+}
+
+export { range, unique,difference };
