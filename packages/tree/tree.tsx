0c2d7e437aef1402bdfdb2cbc577d86d2a7f2bc1
fredafei <964959033@qq.com>
2020-03-31 17:49:24 +0800

    更新tree组件，添加自定义class

--- packages/tree/tree.tsx
+++ packages/tree/tree.tsx
@@ -6,14 +6,19 @@ import TreeItem from './treeItem';
 const componentName = 'Tree';
 const sc = createScopedClasses(componentName);
 
-const Tree: React.FunctionComponent<TreeProps> = props => {
+type Style = {
+  className?: string
+  style?: React.CSSProperties
+}
+
+const Tree: React.FunctionComponent<TreeProps & Style> = props => {
   const { sourceData } = props;
   const onItemChange = (values: string[]) => {
     console.log('tree onItemChange values');
     console.log(values);
     props.onChange(values)
   }
-  return <div className={ classes(sc('')) }>
+  return <div className={ classes(sc(''), props.className) }>
     {
       sourceData.map(item => <TreeItem treeProps={ props } item={ item } leave={ 1 }
                                        key={ item.key } onItemChange={ onItemChange }/>)
