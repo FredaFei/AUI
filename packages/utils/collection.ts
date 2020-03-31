95451d7bfb3683ce1796e6e6f51aaa4dd4ef47ca
fredafei <964959033@qq.com>
2020-03-31 14:48:06 +0800

    添加数组排序/比较大小等函数

--- packages/utils/collection.ts
+++ packages/utils/collection.ts
@@ -11,6 +11,13 @@ function unique(array: any[]) {
   return array.filter(item => obj.hasOwnProperty(typeof item + JSON.stringify(item)) ? false : (obj[typeof item + JSON.stringify(item)] = true));
 }
 
+interface RecursiveArray<T> extends Array<T | RecursiveArray<T>> {}
+
+function flatten(array: RecursiveArray<any>): any[] {
+  if (!array) {return []}
+  return array.reduce((result, next) => result.concat(Array.isArray(next) ? flatten(next) : next), [])
+}
+
 function difference(a: any[], b: any[]) {
   return a.filter(i => b.indexOf(i) === -1);
 }
@@ -19,4 +26,20 @@ function intersection(a: any[], b: any[]) {
   return a.filter(i => b.indexOf(i) > -1);
 }
 
-export { range, unique, difference, intersection };
+function compare(a: string | number, b: string | number) {
+  // 按某种排序标准进行比较, a 小于 b
+  if (a < b) { return -1}
+  if (a > b) {return 1}
+  // a must be equal to b
+  return 0;
+}
+
+function sort(array: Array<string | number>): Array<string | number> {
+  return array.sort(compare)
+}
+
+function towArraysAreEqual(a: Array<number | string>, b: Array<number | string>) {
+  return a.length === b.length && JSON.stringify(sort(a)) === JSON.stringify(sort(b))
+}
+
+export { range, unique, flatten, difference, intersection, sort, towArraysAreEqual };
