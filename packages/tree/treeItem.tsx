e8659bee996398e99bc0bc0118273fab476fd168
fredafei <964959033@qq.com>
2020-04-01 17:03:17 +0800

    更新tree组件，删除console & 注释

--- packages/tree/treeItem.tsx
+++ packages/tree/treeItem.tsx
@@ -20,119 +20,117 @@ const collectionChildrenValues = (item: SourceItem): string[] => {
   return flatten(item.children?.map(i => [i.key, collectionChildrenValues(i)]) as [])
 }
 const TreeItem: React.FunctionComponent<TreeItemProps> = props => {
-    const { item, leave, treeProps } = props;
-    const [expanded, setExpanded] = useState(true);
-    const childrenRef = useRef<HTMLDivElement>(null);
+  const { item, leave, treeProps } = props;
+  const [expanded, setExpanded] = useState(true);
+  const childrenRef = useRef<HTMLDivElement>(null);
 
-    const classes = { [`leave-${ leave }`]: true, 'item': true };
-    useExceptFirstUpdate(expanded, () => {
-      if (!childrenRef.current) {return;}
-      if (expanded) {
-        childrenRef.current.style.position = 'absolute';
-        childrenRef.current.style.opacity = '0';
-        childrenRef.current.style.height = 'auto';
-        const { height } = childrenRef.current.getBoundingClientRect();
-        childrenRef.current.style.position = '';
-        childrenRef.current.style.opacity = '';
-        childrenRef.current.style.height = `0px`;
-        childrenRef.current.getBoundingClientRect();
-        childrenRef.current.style.height = `${ height }px`;
-        childrenRef.current.addEventListener('transitionend', afterExpand);
-      } else {
-        const { height } = childrenRef.current.getBoundingClientRect();
-        childrenRef.current.style.height = `${ height }px`;
-        childrenRef.current.getBoundingClientRect();
-        childrenRef.current.style.height = `0px`;
-        childrenRef.current.addEventListener('transitionend', afterCollapse);
-      }
-
-    });
-    const afterExpand = () => {
-      if (!childrenRef.current) {return;}
-      childrenRef.current.style.height = '';
-      childrenRef.current.removeEventListener('transitionend', afterExpand);
-    };
-    const afterCollapse = () => {
-      if (!childrenRef.current) {return;}
-      childrenRef.current.style.height = '';
-      childrenRef.current.classList.add(sc('gone'));
-      childrenRef.current.removeEventListener('transitionend', afterCollapse);
-    };
-    const expand = () => {
-      setExpanded(true);
-    };
-    const collapse = () => {
-      setExpanded(false);
-    };
-    useEffect(() => {
-      onItemChange(treeProps.selected)
-    }, [])
-    const onItemChange = (values: string[]) => {
-      const childrenValues = collectionChildrenValues(item)
-      const common = intersection(values, childrenValues)
-      if (common.length !== 0) {
-        props.onItemChange(unique(values.concat(item.key)))
-      } else {
-        props.onItemChange(values.filter(v => v !== item.key))
-      }
+  const classes = { [`leave-${ leave }`]: true, 'item': true };
+  useExceptFirstUpdate(expanded, () => {
+    if (!childrenRef.current) {return;}
+    if (expanded) {
+      childrenRef.current.style.position = 'absolute';
+      childrenRef.current.style.opacity = '0';
+      childrenRef.current.style.height = 'auto';
+      const { height } = childrenRef.current.getBoundingClientRect();
+      childrenRef.current.style.position = '';
+      childrenRef.current.style.opacity = '';
+      childrenRef.current.style.height = `0px`;
+      childrenRef.current.getBoundingClientRect();
+      childrenRef.current.style.height = `${ height }px`;
+      childrenRef.current.addEventListener('transitionend', afterExpand);
+    } else {
+      const { height } = childrenRef.current.getBoundingClientRect();
+      childrenRef.current.style.height = `${ height }px`;
+      childrenRef.current.getBoundingClientRect();
+      childrenRef.current.style.height = `0px`;
+      childrenRef.current.addEventListener('transitionend', afterCollapse);
     }
 
-    const onChange = (e: React.FormEvent) => {
-      const checked = (e.target as HTMLInputElement).checked;
-      treeProps.multiple ? multipleSelected(checked) : singleSelected(checked);
-    };
-    const multipleSelected = (checked: boolean) => {
-      const childrenValues = collectionChildrenValues(item)
-      if (checked) {
-        props.onItemChange(unique([...treeProps.selected, item.key, ...childrenValues]))
-      } else {
-        props.onItemChange(treeProps.selected.filter(v => v !== item.key && childrenValues.indexOf(v) === -1))
-      }
-    };
-    const singleSelected = (checked: boolean) => {
-      if (checked) {
-        treeProps.onChange([item.key]);
-      } else {
-        treeProps.onChange([]);
-      }
-    };
-    const checkedAll = () => {
-      const childrenValues = collectionChildrenValues(item)
-      return towArraysAreEqual(childrenValues, intersection(treeProps.selected, childrenValues))
+  });
+  const afterExpand = () => {
+    if (!childrenRef.current) {return;}
+    childrenRef.current.style.height = '';
+    childrenRef.current.removeEventListener('transitionend', afterExpand);
+  };
+  const afterCollapse = () => {
+    if (!childrenRef.current) {return;}
+    childrenRef.current.style.height = '';
+    childrenRef.current.classList.add(sc('gone'));
+    childrenRef.current.removeEventListener('transitionend', afterCollapse);
+  };
+  const expand = () => {
+    setExpanded(true);
+  };
+  const collapse = () => {
+    setExpanded(false);
+  };
+  useEffect(() => {
+    onItemChange(treeProps.selected)
+  }, [])
+  const onItemChange = (values: string[]) => {
+    const childrenValues = collectionChildrenValues(item)
+    const common = intersection(values, childrenValues)
+    if (common.length !== 0) {
+      props.onItemChange(unique(values.concat(item.key)))
+    } else {
+      props.onItemChange(values.filter(v => v !== item.key))
     }
-    const indeterminate = () => {
-      const childrenValues = collectionChildrenValues(item)
-      if (!treeProps.selected.includes(item.key)) {return false}
-      const common = intersection(treeProps.selected, childrenValues)
-      return common.length !== childrenValues.length
+  }
+
+  const onChange = (e: React.FormEvent) => {
+    const checked = (e.target as HTMLInputElement).checked;
+    treeProps.multiple ? multipleSelected(checked) : singleSelected(checked);
+  };
+  const multipleSelected = (checked: boolean) => {
+    const childrenValues = collectionChildrenValues(item)
+    if (checked) {
+      props.onItemChange(unique([...treeProps.selected, item.key, ...childrenValues]))
+    } else {
+      props.onItemChange(treeProps.selected.filter(v => v !== item.key && childrenValues.indexOf(v) === -1))
     }
-    const checkboxRender = () => {
-      const child = item.text instanceof Function ? item.text() : item.text
-      return item.children && treeProps.multiple ?
-        <Checkbox indeterminate={ indeterminate() } checked={ checkedAll() }
-                  onChange={ onChange }>{ child }</Checkbox> :
-        <Checkbox checked={ treeProps.selected.includes(item.key) } value={ item.key }
-                  onChange={ onChange }>{ child }</Checkbox>
+  };
+  const singleSelected = (checked: boolean) => {
+    if (checked) {
+      treeProps.onChange([item.key]);
+    } else {
+      treeProps.onChange([]);
     }
-    const switcherIconRender = () => {
-      return item.children &&
-        <span className={ sc('expand', { collapsed: !expanded }) }>
+  };
+  const checkedAll = () => {
+    const childrenValues = collectionChildrenValues(item)
+    return towArraysAreEqual(childrenValues, intersection(treeProps.selected, childrenValues))
+  }
+  const indeterminate = () => {
+    const childrenValues = collectionChildrenValues(item)
+    if (!treeProps.selected.includes(item.key)) {return false}
+    const common = intersection(treeProps.selected, childrenValues)
+    return common.length !== childrenValues.length
+  }
+  const checkboxRender = () => {
+    const child = item.text instanceof Function ? item.text() : item.text
+    return item.children && treeProps.multiple ?
+      <Checkbox indeterminate={ indeterminate() } checked={ checkedAll() }
+                onChange={ onChange }>{ child }</Checkbox> :
+      <Checkbox checked={ treeProps.selected.includes(item.key) } value={ item.key }
+                onChange={ onChange }>{ child }</Checkbox>
+  }
+  const switcherIconRender = () => {
+    return item.children &&
+      <span className={ sc('expand', { collapsed: !expanded }) }>
           <Icon name="right" className={ sc('expand-icon') } onClick={ expanded ? collapse : expand }/>
         </span>
-    }
-    const itemStyle = { paddingLeft: `${ (leave - 1) * 0.5 }em` }
-    return <div className={ sc(classes) } key={ item.key } style={ itemStyle }>
-      <div className={ sc('text') }>
-        { switcherIconRender() }
-        { checkboxRender() }
-      </div>
-      <div className={ sc('children', { collapsed: !expanded }) } ref={ childrenRef }>
-        { item.children?.map(subItem => <TreeItem treeProps={ treeProps } onItemChange={ onItemChange }
-                                                  item={ subItem } leave={ leave + 1 } key={ subItem.key }/>) }
-      </div>
-    </div>;
-
   }
-;
+  const itemStyle = { paddingLeft: `${ (leave - 1) * 0.5 }em` }
+  return <div className={ sc(classes) } key={ item.key } style={ itemStyle }>
+    <div className={ sc('text') }>
+      { switcherIconRender() }
+      { checkboxRender() }
+    </div>
+    <div className={ sc('children', { collapsed: !expanded }) } ref={ childrenRef }>
+      { item.children?.map(subItem => <TreeItem treeProps={ treeProps } onItemChange={ onItemChange }
+                                                item={ subItem } leave={ leave + 1 } key={ subItem.key }/>) }
+    </div>
+  </div>;
+}
 TreeItem.displayName = componentName;
 export default TreeItem;
